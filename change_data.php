d760b5e602601e083ea2df7cf2125efa515af8f8
Andrey Zhdanov <CayMan.1@bk.ru>
2025-09-02 23:12:11 +0700

    Изменение данных пользователя, добавление комментариев

--- change_data.php
+++ change_data.php
@@ -1,16 +1,21 @@
 <?php
+    //создание сессии, подключение к БД
     session_start();
     $conn=mysqli_connect("localhost", "root", "", "bd_test_zadanie");
 
+    //беру логин из сессии для того, чтобы определить пользователя в БД, чьи данные я буду менять
     $login = $_SESSION['login'];
 
+    //получение данных POST
     $new_username=$_POST['username'];
     $new_email=$_POST['email'];
     $new_number=$_POST['number'];
     $new_password=$_POST['password'];
 
+    //скрипт для изменения данных пользователя, здесь пригодились данные из сессии
     $sql="UPDATE users SET name='$new_username', email='$new_email', number='$new_number', password='$new_password' WHERE email='$login' or number='$login';";
 
+    //обновление данных в БД и сессиях, обновление данных в сессии нужно, чтобы далее пользователь мог заходить в профиль под новым логином
     if($conn->query($sql) === TRUE){
         echo "Данные о пользователе успешно обновлены!";
         $_SESSION['login']=$new_number;
