4aa068b565f4294701ba6e84e7376349e1c88e03
Andrey Zhdanov <CayMan.1@bk.ru>
2025-09-02 23:11:17 +0700

    Страница профиля пользователя, добавление комментариев

--- user_profile.php
+++ user_profile.php
@@ -2,15 +2,16 @@
     $title="Личный кабинет";
     require("blocks/header.php");
     
-
+    //создание сессии, подлючение к БД, а также получение данных из сессии
     session_start();
     $conn=mysqli_connect("localhost", "root", "", "bd_test_zadanie");
     $userlogin = $_SESSION['login'];
     $userpassword = $_SESSION['password'];
+    //проверка на наличие в сесси данных, если данных в сессии нет, то пользователь не сможет войти в профиль
     if($_SESSION['login'] == "" || $_SESSION['password'] == ""){
         header("Location: index.php");
-    }
-    else{
+    } else{
+        //вывод информации о пользователе
         $sql="SELECT * FROM users WHERE (email='$userlogin' OR number='$userlogin') AND password = '$userpassword'";
         $sql_res=mysqli_query($conn, $sql);
 
@@ -20,7 +21,7 @@
             echo "Email пользователя: " . $user_data['email'] . "<br>";
             echo "Телефон пользователя: " . $user_data['number'] . "<br>";
             echo "Пароль пользователя: " . $user_data['password'] . "<br>";
-        }else{
+        } else{
             "Пользователь не найден";
         }
 
@@ -28,7 +29,7 @@
     }
 
 ?>
-
+    <!-- Поля для изменения информации о пользователе, данные передаются на обработку в отдельный файл -->
     <form action="change_data.php" method="post">
         <input type="text" name="username" placeholder="Новое имя";><br>
         <input type="email" name="email" placeholder="Новый email";><br>
@@ -36,6 +37,7 @@
         <input type="password" name="password" placeholder="Новый пароль";><br><br>
         <input type="submit" placeholder="Подтвердить"><br><br>
     </form>
+    <!-- кнопка для выхода из профиля/завершения сессии -->
     <form action="profile_exit.php" method="post">
         <input type="submit" value="Выйти из профиля">
     </form>
