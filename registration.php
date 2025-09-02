9e3738a9be06678e043d71b04a31cdd687eeeba1
Andrey Zhdanov <CayMan.1@bk.ru>
2025-09-02 22:37:28 +0700

    Страница регистрации. добавил комментарии с объяснением кода

--- registration.php
+++ registration.php
@@ -2,16 +2,17 @@
     $title="Регистрация";
     require("blocks/header.php");
 ?>
-
+    <!-- Создал поля для ввода данных пользователя -->
     <form action="reg_check.php" method="post">
         <input type="text" name="username" placeholder="Введите ваше имя" class><br>
         <input type="numeric" name="number" placeholder="Введите ваш номер телефона"><br>
         <input type="email" name="email" placeholder="Введите ваш адрес электронной почты"><br>
         <input type="password" name="password_1" placeholder="Введите пароль"><br>
         <input type="password" name="password_2" placeholder="Введите пароль повторно"><br>
+        <!-- Кнопка для подтверждения отправки данных -->
         <input type="submit" value="Зарегистрироваться">
+        
     </form>
-
 <?php
     require("blocks/footer.php");
 ?>
