6cb2bbbafc8ce6b9be0aa2f195b583805781e88f
Andrey Zhdanov <CayMan.1@bk.ru>
2025-09-04 16:53:14 +0700

    Добавление проверки введенных пользователем данных

--- change_data.php
+++ change_data.php
@@ -5,7 +5,7 @@
     require("connect_bd.php");
 
     //беру логин из сессии для того, чтобы определить пользователя в БД, чьи данные я буду менять
-    $login = $_SESSION['login'];
+    $oldLogin = $_SESSION['login'];
 
     //получение данных POST
     $newEmail = $_POST['email'];
@@ -13,20 +13,48 @@
     $newUsername = $_POST['username'];
     $newPassword = $_POST['password'];
 
-    //скрипт для изменения данных пользователя, здесь пригодились данные из сессии
-    $sql = "UPDATE users SET name = '$newUsername', email = '$newEmail', number = '$newNumber', password = '$newPassword' WHERE email = '$login' or number = '$login';";
-
-    //обновление данных в БД и сессиях, это нужно, чтобы далее пользователь мог заходить в профиль под новым логином
-    try {
-        $conn->query($sql);
-        echo "Данные о пользователе успешно обновлены!";
-        $_SESSION['login'] = $newNumber;
-        $_SESSION['password'] = $newPassword;
-        header("Location: user_profile.php");
-    } catch (Exception $e) {
-        echo "Данные не были обновлены, произошла ошибка: " . $e->getMessage() . "<br>";
-    } finally {
-        echo "<a href='index.php'>Главная страница</a>" . "<br>";
+    if ($newEmail == "" || $newNumber == "" || $newUsername == "" || $newPassword == "") {
+        echo "Заполните все поля и введите все данные!";
         echo "<a href='user_profile.php'>Профиль</a>";
+        die();
     }
+    //проверка на существования пользователя с такими же данные в БД
+    $sqlCheckUser = "SELECT * FROM users WHERE email = '$newEmail' OR number = '$newNumber' OR name='$newUsername'";
+        //записываю в переменную количество полученных строк, от запроса
+        $sqlRow = $conn->query($sqlCheckUser)->num_rows;
+
+        //соответственно, если строк >0, то пользователь уже есть в БД
+        if ($sqlRow > 0) {
+            echo "Пользователь с таким email, номером телефона или логином уже существует" . "<br>";
+            echo "<a href='user_profile.php'>Вернуться в личный кабинет<a>";
+            die();
+        } else if (strlen($newUsername) <= 1) { 
+            echo "Введите корректное имя!" . "<br>";
+            echo "<a href='user_profile.php'>Вернуться в личный кабинет<a>";
+            die();
+        } else if (is_numeric($newNumber) === false) {
+            echo "Вы ввели номер телефона с буквами" . "<br>";
+            echo "<a href='user_profile.php'>Вернуться в личный кабинет<a>";
+            die();
+        } else {
+            //скрипт для изменения данных пользователя, здесь пригодились данные из сессии
+            $sql = "UPDATE users SET name = '$newUsername', email = '$newEmail', number = '$newNumber', password = '$newPassword' WHERE email = '$oldLogin' or number = '$oldLogin';";
+
+            //обновление данных в БД и сессиях, это нужно, чтобы далее пользователь мог заходить в профиль под новым логином
+            try {
+                $conn->query($sql);
+                echo "Данные о пользователе успешно обновлены!";
+                $_SESSION['login'] = $newNumber;
+                $_SESSION['password'] = $newPassword;
+                header("Location: user_profile.php");
+            } catch (Exception $e) {
+                echo "Данные не были обновлены, произошла ошибка: " . $e->getMessage() . "<br>";
+            } finally {
+                echo "<a href='index.php'>Главная страница</a>" . "<br>";
+                echo "<a href='user_profile.php'>Профиль</a>";
+            }
+        }
+        
+
+    
 
