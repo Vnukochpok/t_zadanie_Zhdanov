1abbcef7b63ef899954befa5cce94150b669f9e0
Andrey Zhdanov <CayMan.1@bk.ru>
2025-09-04 16:00:40 +0700

    Использование try|catch|finally вместо if|else

--- change_data.php
+++ change_data.php
@@ -2,7 +2,7 @@
 
     //создание сессии, подключение к БД
     session_start();
-    require("connect_bd_php");
+    require("connect_bd.php");
 
     //беру логин из сессии для того, чтобы определить пользователя в БД, чьи данные я буду менять
     $login = $_SESSION['login'];
@@ -16,14 +16,17 @@
     //скрипт для изменения данных пользователя, здесь пригодились данные из сессии
     $sql = "UPDATE users SET name = '$newUsername', email = '$newEmail', number = '$newNumber', password = '$newPassword' WHERE email = '$login' or number = '$login';";
 
-    //обновление данных в БД и сессиях, обновление данных в сессии нужно, чтобы далее пользователь мог заходить в профиль под новым логином
-    if ($conn->query($sql) === true) {
+    //обновление данных в БД и сессиях, это нужно, чтобы далее пользователь мог заходить в профиль под новым логином
+    try {
+        $conn->query($sql);
         echo "Данные о пользователе успешно обновлены!";
         $_SESSION['login'] = $newNumber;
         $_SESSION['password'] = $newPassword;
         header("Location: user_profile.php");
-    } else {
-        echo "Данные не были обновлены, произошла ошибка" . "<br>";
+    } catch (Exception $e) {
+        echo "Данные не были обновлены, произошла ошибка: " . $e->getMessage() . "<br>";
+    } finally {
         echo "<a href='index.php'>Главная страница</a>" . "<br>";
         echo "<a href='user_profile.php'>Профиль</a>";
     }
+
